4dae5c6d4fcce702bac3336b3187cf84ed21fd9a
mspstead <mspstead@gmail.com>
2016-02-23 08:20:20 -0500

    initial_project_version

---
+++ django_project/django_project/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'django_project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    
+    url(r'^flood/', include('flood.urls')),
+    url(r'^admin/', admin.site.urls),
+)
