fb48a75bbddeb4b1f67154b6d024b56a49571c0c
mspstead <mspstead@gmail.com>
2016-02-23 20:40:44 +0000

    edited url for map view

--- django_project/flood/views.py
+++ django_project/flood/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello Michael <br/> <a href='map/'>Map</a>")
+    return HttpResponse("Hello Michael <br/> <a href='map'>Map</a>")
 
 
 def map(request):
