756639f50d4fc6e6967536ca5c657764cdd41901
mspstead <mspstead@gmail.com>
2016-02-23 13:45:48 +0000

    Test push from local pc.

--- django_project/flood/views.py
+++ django_project/flood/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, Mike. I am awesome")
+    return HttpResponse("Hello, Mike. I am amazing.")
