a38311760870b3d83b67fbed3dec66b07e98b140
mspstead <mspstead@gmail.com>
2016-02-23 18:37:56 +0000

    test of adding second view

--- django_project/flood/views.py
+++ django_project/flood/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, Mike. I am amazing.")
+    return HttpResponse("Hello Michael <br/> <a href='/map'>Map</a>")
+
+
+def map(request):
+    return HttpResponse("This is map page <a href='/r^$'>Index</a>")
