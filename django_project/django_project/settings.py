262a8c26f8b37e9a4de0fb23309366fa9a795d2b
mspstead <mspstead@gmail.com>
2016-02-24 14:24:30 -0500

    Edited settings.py to add leaflet and bootstrap to installed_apps

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'leaflet',
+    'twitter_bootstrap',
 )
 
 MIDDLEWARE_CLASSES = (
