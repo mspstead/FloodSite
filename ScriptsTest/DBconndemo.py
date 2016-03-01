5c928f44da2fb663d3bfa9a5471f75ef6d63301d
mspstead <mspstead@gmail.com>
2016-03-01 09:40:50 -0500

    Edited db conn file.

--- ScriptsTest/DBconndemo.py
+++ ScriptsTest/DBconndemo.py
@@ -1,7 +1,7 @@
 import psycopg2
 
 try:
-    conn = psycopg2.connect(database='django', user='', password='', host='localhost')
+    conn = psycopg2.connect(database='django', user='django', password='AgZP7U56Mw', host='localhost')
 except:
     print("Unable to connect to the database.")
 
