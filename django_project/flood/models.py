a9a7c50fe84c9ec2845d84890818bb7d02c6f8e5
mspstead <mspstead@gmail.com>
2016-03-01 15:15:58 +0000

    Added initial photo model.

--- django_project/flood/models.py
+++ django_project/flood/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Photo(models.Model):
+    owner = models.TextField()
+    title_text = models.TextField()
+    date_taken = models.DateTimeField('date taken')
+    url = models.TextField()
+    lat = models.TextField()
+    lng = models.TextField()
+    locality = models.TimeField()
