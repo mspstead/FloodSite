f5dc7cb0a11b8b7deabdf7368d7f8b731e107fb5
mspstead <mspstead@gmail.com>
2016-03-02 15:59:13 +0000

    added map template to map view.

--- django_project/flood/views.py
+++ django_project/flood/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
 # Create your views here.
 
@@ -8,5 +9,4 @@ def index(request):
 
 
 def map(request):
-    template = loader.get_template('/templates/map.html')
-    return HttpResponse(template.render(request))
+    return render(request, 'flood/map.html')
