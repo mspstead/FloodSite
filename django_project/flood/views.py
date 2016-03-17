a151463a7d15ae8fe5c335f05ee249a8b26783d8
mspstead <mspstead@gmail.com>
2016-03-17 13:44:30 +0000

    Added distinct command to timeline view.

--- django_project/flood/views.py
+++ django_project/flood/views.py
@@ -15,6 +15,6 @@ def map(request):
     return render(request, 'flood/map.html', context)
 
 def timeline(request):
-    ordered_photo_list = Photo.objects.order_by("date_taken")
+    ordered_photo_list = Photo.objects.order_by("date_taken").distinct("locality")
     context = {'photo_list':ordered_photo_list}
     return render(request, 'flood/timeline.html', context)
