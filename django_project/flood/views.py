c7eef6564be79c5c7cd37df09bfd7d0bdecce6bd
mspstead <mspstead@gmail.com>
2016-04-01 16:27:59 +0100

    First attempt at grouping photos on timeline based on flood event rather than locality, fixing intial bugs.

--- django_project/flood/views.py
+++ django_project/flood/views.py
@@ -22,7 +22,7 @@ def timeline(request):
     for x in range(1, len(ordered_photo_list)):
         flood_event = []
         difference = abs(ordered_photo_list[x].date_taken - start_date).days
-        if difference < datetime.datetime.strptime('3','%d'):
+        if difference < 3:
             flood_event.append(ordered_photo_list[x-1])
         else:
             flood_events.append(flood_event)
