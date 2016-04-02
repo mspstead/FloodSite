9e9bc43a10062d4b8f48f1c00808a69588507e76
mspstead <mspstead@gmail.com>
2016-04-02 20:14:45 +0100

    Fixing flood events in else statement

--- django_project/flood/views.py
+++ django_project/flood/views.py
@@ -30,13 +30,9 @@ def timeline(request):
             flood_event.append(ordered_photo_list[x]) #if photos within 1 day of each other add to flood event
             start_date = ordered_photo_list[x].date_taken.date()
         else:
-            if(len(flood_event) > 1):
-                flood_events.append(flood_event) #add flood event to the flood_events list
-                flood_event = []
-            else:
-                flood_event.append(ordered_photo_list[x])
-                flood_events.append(flood_event) #add flood event to the flood_events list
-                flood_event = [] #reset flood event to empty
+            flood_event.append(ordered_photo_list[x])
+            flood_events.append(flood_event) #add flood event to the flood_events list
+            flood_event = [] #reset flood event to empty
         start_date = ordered_photo_list[x].date_taken.date() #set new start_date to the next photo date in list.
     context = {'photo_list':ordered_photo_list, 'flood_events':flood_events}
     return render(request, 'flood/timeline.html', context)
