aab5fb1d356fc8a1a8cf27c5a9d43783a509301a
mspstead <mspstead@gmail.com>
2016-04-02 14:30:10 +0100

    added comments and moved start_date to inside else statement.

--- django_project/flood/views.py
+++ django_project/flood/views.py
@@ -16,18 +16,18 @@ def map(request):
     return render(request, 'flood/map.html', context)
 
 def timeline(request):
-    ordered_photo_list = Photo.objects.order_by("date_taken")
-    flood_events = []
-    start_date = ordered_photo_list[0].date_taken
-    flood_event = []
-    for x in range(0, len(ordered_photo_list)):
-        difference = (ordered_photo_list[x].date_taken - start_date).days
-        if difference < 7:
-            flood_event.append(ordered_photo_list[x])
+    ordered_photo_list = Photo.objects.order_by("date_taken") #order the photos based on the date_taken
+    flood_events = [] #empty list to hold all of the flood events
+    start_date = ordered_photo_list[0].date_taken #make start_date for flood events equal to first flood photo date
+    flood_event = [] #list to hold individual flood event photo objects
+    for x in range(0, len(ordered_photo_list)): #cycle through the list
+        difference = (ordered_photo_list[x].date_taken - start_date).days #calculate the difference between dates between photos
+        if difference < 7: #check the difference is less than 7 days
+            flood_event.append(ordered_photo_list[x]) #if photos within 7 days of each other add to flood event
         else:
-            flood_events.append(flood_event)
-            flood_event = []
-        start_date = ordered_photo_list[x].date_taken
+            flood_events.append(flood_event) #add flood event to the
+            flood_event = [] #reset flood event to empty
+            start_date = ordered_photo_list[x].date_taken #set new start_date to current photo date in list.
     context = {'photo_list':ordered_photo_list, 'flood_events':flood_events}
     return render(request, 'flood/timeline.html', context)
 
