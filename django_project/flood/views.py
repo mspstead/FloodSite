49f0beed8e39b82ccffcf27209ca00e19004cea2
mspstead <mspstead@gmail.com>
2016-04-02 16:03:14 +0100

    changing start_date in if statement to be set to next photos start date.

--- django_project/flood/views.py
+++ django_project/flood/views.py
@@ -19,17 +19,17 @@ def timeline(request):
     ordered_photo_list = Photo.objects.order_by("date_taken") #order the photos based on the date_taken
     flood_events = [] #empty list to hold all of the flood events
     start_date = ordered_photo_list[0].date_taken #Initialise start_date
+
     flood_event = [] #list to hold individual flood event photo objects
     for x in range(0, len(ordered_photo_list)): #cycle through the list
         difference = (ordered_photo_list[x].date_taken - start_date).days #calculate the difference between dates between photos
-        if difference <= 3: #check the difference between dates is less than or equal to 1 day
+        if difference <= 1: #check the difference between dates is less than or equal to 1 day
             flood_event.append(ordered_photo_list[x]) #if photos within 1 day of each other add to flood event
-            start_date = ordered_photo_list[x].date_taken #set new start_date to current photo date in list.
-
         else:
+            flood_event.append(ordered_photo_list[x])
             flood_events.append(flood_event) #add flood event to the flood_events list
             flood_event = [] #reset flood event to empty
-            start_date = ordered_photo_list[x+1].date_taken #set new start_date to the next photo date in list.
+        start_date = ordered_photo_list[x].date_taken #set new start_date to the next photo date in list.
     context = {'photo_list':ordered_photo_list, 'flood_events':flood_events}
     return render(request, 'flood/timeline.html', context)
 
