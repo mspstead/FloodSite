30b3d75284e8bb87cd58b5dfd1d3ed2bf9b6a211
mspstead <mspstead@gmail.com>
2016-04-03 15:35:30 +0100

    added .date() to filter as was comparing string to date vs date to date.

--- django_project/flood/views.py
+++ django_project/flood/views.py
@@ -40,7 +40,7 @@ def getFloodEvents(list):
             start_date = ordered_list[x].date_taken.date()
         else:
             flood_event.append(ordered_list[x-1])
-            rain_levels = RainLevel.objects.filter(date_taken__gte=flood_event[0]).filter(date_taken__lte=flood_event[len(flood_event)])
+            rain_levels = RainLevel.objects.filter(date_taken__gte=flood_event[0].date(), date_taken__lte=flood_event[len(flood_event)].date())
             flood_events.append([flood_event[0],flood_event[len(flood_event)]],[rain_levels]) #add flood event to the flood_events list
             flood_event = [] #reset flood event to empty
         start_date = ordered_list[x].date_taken.date() #set new start_date to the next photo date in list.
