405c2ab03c28ce7582ac112fc391352599f5e45f
mspstead <mspstead@gmail.com>
2016-04-03 12:23:26 +0100

    Added db connection to populate the db with rain levels taken from potteryfields raingauge.

--- Scripts/RainGaugeExtract.py
+++ Scripts/RainGaugeExtract.py
@@ -1,4 +1,5 @@
 import csv
+import psycopg2
 
 def openCsv(file):
     with open(file) as csvfile:
@@ -8,6 +9,26 @@ def openCsv(file):
             level = float(row['mm'])
             if level > 0 and level < 3:
                 rainLevels.append([row['DateTime'],row['mm']])
+
+    return rainLevels
+
+def populateDB(rainLevels):
+    if len(rainLevels) > 1:
+
+        try:
+            conn = psycopg2.connect(database='django', user='django', password='AgZP7U56Mw', host='http://46.101.11.228')
+            curr = conn.cursor()
+        except:
+            print("Unable to connect")
+
         for rain in rainLevels:
-            print(rain)
-openCsv('RainGaugePtryF.csv')
+            dateValue = rain[0]
+            mm = rain[1]
+            ref = "Pottery Fields"
+            curr.execute("INSERT INTO flood_rainlevel (date_taken,level,reference) "
+                              "VALUES (%s,%s,%s)",(dateValue,mm,ref))
+            print("ADDED:",rain)
+
+
+levels = openCsv('RainGaugePtryF.csv')
+populateDB(levels)
