899fe57a59aad569b73d2047a258221b2ad3b7fc
mspstead <mspstead@gmail.com>
2016-04-04 17:09:27 +0100

    Adding list display to admin.

--- django_project/flood/admin.py
+++ django_project/flood/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Photo, RainLevel
-admin.site.register(Photo)
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('date_taken', 'locality')
+
+admin.site.register(Photo, PhotoAdmin)
 admin.site.register(RainLevel)
