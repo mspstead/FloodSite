93dec0e865cc8b0fda8212df08c80d9d259df472
mspstead <mspstead@gmail.com>
2016-04-07 17:10:28 +0100

    Adding new filter to admin to filter by photos with negative/postive scores, this should make it easier to delete non-relevant negatively scored photos.

--- django_project/flood/admin.py
+++ django_project/flood/admin.py
@@ -1,14 +1,47 @@
 from django.contrib import admin
 from django.contrib.admin import SimpleListFilter
+from django.utils.translation import ugettext_lazy as _
+
+
 # Register your models here.
 from .models import Photo, RainLevel
 
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ('date_taken', 'locality')
-    list_filter = ('locality',)
+    list_filter = ('locality',PhotoListFilter)
 
 class PhotoListFilter(SimpleListFilter):
-    print("")
+
+    title = _('Negatively Rated')
+    parameter_name = 'score'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return (
+            ('negative', _('negative')),
+            ('positive', _('postive')),
+        )
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        # Compare the requested value (either '80s' or '90s')
+        # to decide how to filter the queryset.
+        if self.value() == 'negative':
+            return queryset.filter(Photo__lt=0)
+        if self.value() == 'postive':
+            return queryset.filter(Photo__gt=0)
+
+
 
 admin.site.register(Photo, PhotoAdmin)
 admin.site.register(RainLevel)
