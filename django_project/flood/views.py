a2921dd86dc769b751e125008ad0d5edd143103c
mspstead <mspstead@gmail.com>
2016-04-07 16:55:05 +0100

    fixing return method of up/downvote buttons.

--- django_project/flood/views.py
+++ django_project/flood/views.py
@@ -35,12 +35,13 @@ def upvote(request, photo_id):
     photo = Photo.objects.get(pk=photo_id)
     photo.score += 1
     photo.save()
-
+    return HttpResponse('flood/timeline.html')
 
 def downvote(request, photo_id):
     photo = Photo.objects.get(pk=photo_id)
     photo.score -= 1
     photo.save()
+    return HttpResponse('flood/timeline.html')
 
 def getFloodEvents(list):
     ordered_list = list
