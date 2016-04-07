7954ae106bd8fa21230b6a323f4ebf03f381fa27
mspstead <mspstead@gmail.com>
2016-04-07 17:27:35 +0100

    Fixing simplelist filter.

--- django_project/flood/admin.py
+++ django_project/flood/admin.py
@@ -6,13 +6,9 @@ from django.utils.translation import ugettext_lazy as _
 # Register your models here.
 from .models import Photo, RainLevel
 
-class PhotoAdmin(admin.ModelAdmin):
-    list_display = ('date_taken', 'locality', 'score')
-    list_filter = (PhotoListFilter)
-
 class PhotoListFilter(SimpleListFilter):
 
-    title = _('Negatively Rated')
+    title = _('Scored photos')
     parameter_name = 'score'
 
     def lookups(self, request, model_admin):
@@ -37,11 +33,13 @@ class PhotoListFilter(SimpleListFilter):
         # Compare the requested value (either '80s' or '90s')
         # to decide how to filter the queryset.
         if self.value() == 'negative':
-            return Photo.objects.filter(score__lt=0)
+            return queryset.filter(score__lt=0)
         if self.value() == 'postive':
-            return Photo.objects.filter(score__gt=0)
+            return queryset.filter(score__gt=0)
 
-
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('date_taken', 'locality', 'score')
+    list_filter = (PhotoListFilter)
 
 admin.site.register(Photo, PhotoAdmin)
 admin.site.register(RainLevel)
