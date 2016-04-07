21340b62e264120fdf5ce03241bf367ee61442ed
mspstead <mspstead@gmail.com>
2016-04-07 16:23:38 +0100

    Changed {% photo.id %} to {{ photo.id }}

--- django_project/flood/urls.py
+++ django_project/flood/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^map', views.map, name="map"),
     url(r'^timeline', views.timeline, name="timeline"),
     url(r'^graph', views.graph, name="graph"),
-    url(r'^downvote/(?P<photo_id>[0-9]+)/$', views.upvote, name='upvote'),
+    url(r'^upvote/(?P<photo_id>[0-9]+)/$', views.upvote, name='upvote'),
     url(r'^downvote/(?P<photo_id>[0-9]+)/$', views.downvote, name='downvote')
 
 ]
