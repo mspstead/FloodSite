ece7fc1d6aae5593a8b3f0413af62c8d993b6d27
mspstead <mspstead@gmail.com>
2016-04-08 12:15:08 +0100

    fixing index url in flood/urls

--- django_project/flood/urls.py
+++ django_project/flood/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^index', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^map', views.map, name="map"),
     url(r'^timeline', views.timeline, name="timeline"),
     url(r'^graph', views.graph, name="graph"),
