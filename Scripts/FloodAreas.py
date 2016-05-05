97a9c3f6ec135b70ff4ab61f8eee7424f7e19c03
mspstead <mspstead@gmail.com>
2016-05-05 17:04:25 +0100

    Added comments, also added test data to FloodAreas

--- Scripts/FloodAreas.py
+++ Scripts/FloodAreas.py
@@ -5,7 +5,7 @@ import requests
 import simplejson as json
 import InstagramDailyScript
 import time
-import DB
+from DB import DB
 
 def searchArea(lat,lng,rad):
     """
@@ -19,14 +19,18 @@ def searchArea(lat,lng,rad):
     searchUrl = "http://environment.data.gov.uk/flood-monitoring/id/floods?lat="+lat+"&long="+lng+"&dist="+rad
 
     req = requests.get(searchUrl)
-    #print(req.text)
-    jsonData = json.loads(req.text) #load the json data returned from the request
+
+    #test json data used to check script is working properly
+    #testJson = '{"@context" : "http://environment.data.gov.uk/flood-monitoring/meta/context.jsonld" , "meta" : { "publisher" : "Environment Agency" ,"licence" : "http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" ,"documentation" : "http://environment.data.gov.uk/flood-monitoring/doc/reference" ,"version" : "0.6" ,"comment" : "Status: Beta service" ,"hasFormat" : [ "http://environment.data.gov.uk/flood-monitoring/id/floods.csv" ]},"items" : [ {"@id":"http://environment.data.gov.uk/flood-monitoring/id/floodAreas/053FWFPUWI06","description": "flood warning for leeds","severity": "Warning","severityLevel": "4","timeRaised": "01/01/2016 16:00:00"}, {"@id":"http://environment.data.gov.uk/flood-monitoring/id/floodAreas/061FWF23Goring", "description": "flood warning for Hebden Bride", "severity": "Alert", "severityLevel": "3", "timeRaised": "01/01/2016 15:00:00"} ]}'
+
+    jsonData = json.loads(req.text) #load the json data returned from the request, or use testJson
 
     floodAreaInfo = [] #holds all the individual urls relating to any flooding in the search area
     for item in jsonData['items']:
         floodAreaInfo.append({'id':item["@id"], 'description':item["description"],
                               'severity':item["severity"], 'severitylevel':item["severityLevel"],
-                              'time':item["timeRaised"]})
+                              'time':item["timeRaised"]}) #add flood area dictionary to array
+    #print floodAreaInfo
 
     return floodAreaInfo
 
@@ -40,26 +44,31 @@ def getFloodLocation(floodUrl):
     print(floodUrl)
     req = requests.get(floodUrl)
     jsonData = json.loads(req.text)
-    latitude = jsonData["items"]["floodArea"]["lat"] #get the latitude of the flood from the json data
-    longitude = jsonData["items"]["floodArea"]["long"] #get the longitude of the flood
+    latitude = jsonData["items"]["lat"] #get the latitude of the flood from the json data
+    longitude = jsonData["items"]["long"] #get the longitude of the flood
     location.append(latitude)
     location.append(longitude)
 
     return location
 
 def runInstagram(floodInfo):
+    """
+    run the instagram script on flood area lat and lng and add found data to DB
+    :param floodInfo: floodArea url
+    :return:
+    """
 
     DBUpdate = DB()
 
     for flood in floodInfo: #cycle through all the flooded areas urls
 
         location = getFloodLocation(flood.get("id")) #get the flooded areas lat/lng
-        print(location)
+        print("location:",location)
 
         #search for any instagram posts relating to those locations and specified search term
         instaArray = InstagramDailyScript.searchLocation(str(location[0]),str(location[1]),"flood")
 
-        print(instaArray)
+        print("Instagram Data:",instaArray)
 
         DBUpdate.addphotodatabase(instaArray)
 
@@ -71,6 +80,7 @@ if floodInfo != []: #check that there has been flood alerts
     for i in range(0,144): #cycle through 144 times as 144*600seconds = 24hours
 
         runInstagram(floodInfo) #run instagram script every 10minutes for 24hours, gets recent media
+        print("sleeping 10minutes")
         time.sleep(600) #sleep for 10minutes
 else:
     print("No flood alerts")
--- Scripts/RiverLevelScript.py
+++ Scripts/RiverLevelScript.py
@@ -4,8 +4,9 @@ from datetime import date, timedelta
 from time import strftime
 from DB import DB
 
-base_url = "http://environment.data.gov.uk/flood-monitoring/archive/readings-"
+base_url = "http://environment.data.gov.uk/flood-monitoring/archive/readings-" #Url used to request csv files
 
+#Measure Urls used to filter the data
 leeds = "http://environment.data.gov.uk/flood-monitoring/id/measures/L1707-level-stage-i-15_min-m"
 kirkstall = "http://environment.data.gov.uk/flood-monitoring/id/measures/L1708-level-stage-i-15_min-m"
 castleford = "http://environment.data.gov.uk/flood-monitoring/id/measures/L1703-level-stage-i-15_min-m"
@@ -13,80 +14,93 @@ tadcaster = "http://environment.data.gov.uk/flood-monitoring/id/measures/L1304-l
 mytholmroyd = "http://environment.data.gov.uk/flood-monitoring/id/measures/L1204-level-stage-i-15_min-m"
 
 def DownloadRiverData(start_date, end_date):
+    """
+    Gets river level data for particular days and locations, adds to database and/or save to file
+    :param start_date: date object, Y m d
+    :param end_date: date object, Y m d
+    :return:
+    """
 
-    delta = end_date - start_date
+    delta = end_date - start_date #calculate the number of days between the dates
 
-    river_file = open('YorkshireRiverLevels.txt', 'w')
-    levels = []
-    for x in range(delta.days +1):
+    river_file = open('YorkshireRiverLevels.txt', 'w') #text file which processed data will be saved to.
+    levels = [] #empty array to hold river level dictionary objects
 
-        datecsv =  start_date + timedelta(days=x)
-        requrl = base_url + datecsv.strftime("%Y-%m-%d") + ".csv"
+    for x in range(delta.days +1): #cycle through the days
+
+        datecsv =  start_date + timedelta(days=x) #the date which is to be queried
+        requrl = base_url + datecsv.strftime("%Y-%m-%d") + ".csv" #create the url used to request the csv file
         #print(requrl)
-        request =  requests.get(requrl)
+        request = requests.get(requrl) #request the file
 
-        csv_data = request.iter_lines()
+        csv_data = request.iter_lines() #read the csv file
         reader = csv.DictReader(csv_data)
 
-        for row in reader:
+        for row in reader: #cycle through each line in the csv file
 
-            river_level = {}
+            river_level = {} #initialise river level dictionary
 
-            measure = row['measure']
+            measure = row['measure'] #get the measure URL, date and river level
             date = row['dateTime']
             value = row['value']
 
-            if measure == leeds:
+            if measure == leeds: #check if measure Url = leeds measure url and add relevant data to dictionary
                 river_level = {"measure":"Leeds", "datetime":date, "level":value, "lat":"53.794197", "lng":"-1.535433"}
-                levels.append(river_level)
+                levels.append(river_level) #add to array
 
-            elif measure == kirkstall:
+            elif measure == kirkstall: #check if measure Url = kirkstall measure url and add relevant data to dictionary
                 river_level = {"measure":"Kirkstall", "datetime":date, "level":value, "lat":"53.819613", "lng":"-1.605025"}
                 levels.append(river_level)
 
-            elif measure == castleford:
+            elif measure == castleford: #check if measure Url = castleford measure url and add relevant data to dictionary
                 river_level = {"measure":"castleford", "datetime":date, "level":value, "lat":"53.731002", "lng":"-1.358177"}
                 levels.append(river_level)
 
-            elif measure == tadcaster:
+            elif measure == tadcaster: #check if measure Url = tadcaster measure url and add relevant data to dictionary
                 river_level = {"measure":"tadcaster", "datetime":date, "level":value, "lat":"53.677173", "lng":"-1.491306"}
                 levels.append(river_level)
 
-            elif measure == mytholmroyd:
+            elif measure == mytholmroyd: #check if measure Url = mytholmroyd measure url and add relevant data to dictionary
                 river_level = {"measure":"mytholmroyd", "datetime":date, "level":value, "lat":"53.73128", "lng":"-1.983282"}
                 levels.append(river_level)
 
-            if river_level != {}:
+            if river_level != {}: #if the dictionary contains data write the data to the file
 
                 river_file.write(river_level.get("measure")+","+river_level.get("datetime")+","+
                     river_level.get("level")+","+river_level.get("lat")+","+river_level.get("lng")+"\n")
 
             #print (river_level)
-        print(datecsv.strftime("%Y-%m-%d")+", completed.")
+        print(datecsv.strftime("%Y-%m-%d")+", completed.") #output the completed day
     DBupdate = DB()
-    DBupdate.addRiverLevel(levels)
-    river_file.close()
+    DBupdate.addRiverLevel(levels) #add data to database using levels array
+    river_file.close() #close the file
 
 def readFromFile(fileName):
+    """
+    Saves river-level data from file to database
+    :param fileName: file produced by DownloadRiverData()
+    :return:
+    """
 
-    data = open(fileName, 'r')
-    levels = []
-    for line in data:
-        content = line.split(",")
+    data = open(fileName, 'r') #open the file
+    levels = [] #create an array to hold the river level dictionaries
+
+    for line in data: #examine each line in file
+        content = line.split(",") #seperate data by comma and get the relevant valyes
         place = content[0]
         date_taken = content[1]
         level = content[2]
         lat = content[3]
         lng = content[4]
-        river_level = {"measure":place, "datetime":date_taken, "level":level, "lat":lat, "lng":lng}
-        levels.append(river_level)
+        river_level = {"measure":place, "datetime":date_taken, "level":level, "lat":lat, "lng":lng} #save data to dictionary
+        levels.append(river_level) #add to array
     DBupdate = DB()
-    DBupdate.addRiverLevel(levels)
+    DBupdate.addRiverLevel(levels) #add data to database using levels array
 
-strt_date = date(2015,11,14)
-ed_date = date(2016,01,10)
+strt_date = date(2015,11,14) #set the start date, earliest start date of 15/01/2015
+ed_date = date(2016,01,10) #set the end date, max end date of  yesterdays date
 
-#DownloadRiverData(strt_date,ed_date)
-readFromFile("YorkshireRiverLevels.txt")
+#DownloadRiverData(strt_date,ed_date) # uncomment to execute either function
+#readFromFile("YorkshireRiverLevels.txt")
 
 
